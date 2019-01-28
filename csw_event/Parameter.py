1f787bc36f9e180fff0e85a6ab3354948dacfaaf
abrighton <78px3WT5s>
2019-01-28 15:35:12 +0100

    Added other param types, in progress

--- csw_event/Parameter.py
+++ csw_event/Parameter.py
@@ -1,47 +1,68 @@
-from csw_protobuf.keytype_pb2 import IntKey, StringKey
+from csw_protobuf.keytype_pb2 import IntKey, StringKey, ChoiceKey, RaDecKey, StructKey, BooleanKey, ByteKey, CharKey, \
+    ShortKey, LongKey, FloatKey, DoubleKey, TimestampKey, ByteArrayKey, ShortArrayKey, LongArrayKey, IntArrayKey, \
+    FloatArrayKey, DoubleArrayKey, ByteMatrixKey, ShortMatrixKey, LongMatrixKey, IntMatrixKey, FloatMatrixKey, \
+    DoubleMatrixKey
 from csw_protobuf.parameter_pb2 import PbParameter
 from csw_protobuf.units_pb2 import NoUnits
 
 class Parameter:
 
-# XXX TODO: Add support for all these item types in the two tables below
-    # ChoiceKey = 0
-    # RaDecKey = 1
-    # StringKey = 2
-    # StructKey = 3
-    # BooleanKey = 4
-    # ByteKey = 5
-    # CharKey = 6
-    # ShortKey = 7
-    # LongKey = 8
-    # IntKey = 9
-    # FloatKey = 10
-    # DoubleKey = 11
-    # TimestampKey = 12
-    # ByteArrayKey = 13
-    # ShortArrayKey = 14
-    # LongArrayKey = 15
-    # IntArrayKey = 16
-    # FloatArrayKey = 17
-    # DoubleArrayKey = 18
-    # ByteMatrixKey = 19
-    # ShortMatrixKey = 20
-    # LongMatrixKey = 21
-    # IntMatrixKey = 22
-    # FloatMatrixKey = 23
-    # DoubleMatrixKey = 24
-
-
     # Depending on the key type, set a different field in the parameter p to the list of items i.
     __itemSetters__ = {
+        ChoiceKey: lambda p, i: p.choiceItems.values.extend(i),
+        RaDecKey: lambda p, i: p.raDecItems.values.extend(i),
         StringKey: lambda p, i: p.stringItems.values.extend(i),
+        StructKey: lambda p, i: p.structItems.values.extend(i),
+        BooleanKey: lambda p, i: p.booleanItems.values.extend(i),
+        ByteKey: lambda p, i: p.byteItems.values.extend(i),
+        CharKey: lambda p, i: p.charItems.values.extend(i),
+        ShortKey: lambda p, i: p.shortItems.values.extend(i),
+        LongKey: lambda p, i: p.longItems.values.extend(i),
         IntKey: lambda p, i: p.intItems.values.extend(i),
+        FloatKey: lambda p, i: p.floatItems.values.extend(i),
+        DoubleKey: lambda p, i: p.doubleItems.values.extend(i),
+        TimestampKey: lambda p, i: p.instantItems.values.extend(i),
+        ByteArrayKey: lambda p, i: p.byteArrayItems.values.extend(i),
+        ShortArrayKey: lambda p, i: p.shortArrayItems.values.extend(i),
+        LongArrayKey: lambda p, i: p.longArrayItems.values.extend(i),
+        IntArrayKey: lambda p, i: p.intArrayItems.values.extend(i),
+        FloatArrayKey: lambda p, i: p.floatArrayItems.values.extend(i),
+        DoubleArrayKey: lambda p, i: p.doubleArrayItems.values.extend(i),
+        ByteMatrixKey: lambda p, i: p.byteMatrixItems.values.extend(i),
+        ShortMatrixKey: lambda p, i: p.shortMatrixItems.values.extend(i),
+        LongMatrixKey: lambda p, i: p.longMatrixItems.values.extend(i),
+        IntMatrixKey: lambda p, i: p.intMatrixItems.values.extend(i),
+        FloatMatrixKey: lambda p, i: p.floatMatrixItems.values.extend(i),
+        DoubleMatrixKey: lambda p, i: p.doubleMatrixItems.values.extend(i),
     }
 
     # Depending on the key type, get a different field in the parameter p as a list of items.
     __itemGetters__ = {
+        ChoiceKey: lambda p: p.choiceItems.values,
+        RaDecKey: lambda p: p.raDecItems.values,
         StringKey: lambda p: p.stringItems.values,
+        StructKey: lambda p: p.structItems.values,
+        BooleanKey: lambda p: p.booleanItems.values,
+        ByteKey: lambda p: p.byteItems.values,
+        CharKey: lambda p: p.charItems.values,
+        ShortKey: lambda p: p.shortItems.values,
+        LongKey: lambda p: p.longItems.values,
         IntKey: lambda p: p.intItems.values,
+        FloatKey: lambda p: p.floatItems.values,
+        DoubleKey: lambda p: p.doubleItems.values,
+        TimestampKey: lambda p: p.instantItems.values,
+        ByteArrayKey: lambda p: p.byteArrayItems.values,
+        ShortArrayKey: lambda p: p.shortArrayItems.values,
+        LongArrayKey: lambda p: p.longArrayItems.values,
+        IntArrayKey: lambda p: p.intArrayItems.values,
+        FloatArrayKey: lambda p: p.floatArrayItems.values,
+        DoubleArrayKey: lambda p: p.doubleArrayItems.values,
+        ByteMatrixKey: lambda p: p.byteMatrixItems.values,
+        ShortMatrixKey: lambda p: p.shortMatrixItems.values,
+        LongMatrixKey: lambda p: p.longMatrixItems.values,
+        IntMatrixKey: lambda p: p.intMatrixItems.values,
+        FloatMatrixKey: lambda p: p.floatMatrixItems.values,
+        DoubleMatrixKey: lambda p: p.doubleMatrixItems.values,
     }
 
     def __init__(self, keyName, keyType, items, units = NoUnits):
