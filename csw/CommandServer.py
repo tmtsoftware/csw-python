ccba2c80b48c283df029745c72f1471308e6fdf5
abrighton <78px3WT5s>
2019-12-16 13:39:38 +0100

    Added QueryFinal handling (No timeout handling yet)

--- csw/CommandServer.py
+++ csw/CommandServer.py
@@ -1,11 +1,14 @@
+import json
 from dataclasses import dataclass
 
-from aiohttp import web
+import aiohttp
+from aiohttp import web, WSMessage
 from aiohttp.web_request import Request
 from aiohttp.web_response import Response
 import atexit
 import uuid
 
+from aiohttp.web_ws import WebSocketResponse
 from dataclasses_json import dataclass_json
 
 from csw.CommandResponseManager import CommandResponseManager
@@ -34,8 +37,20 @@ class Oneway:
 
 @dataclass
 @dataclass_json
-class Query:
+class QueryFinal:
     runId: str
+    timeout: str
+
+    @staticmethod
+    def fromDict(obj):
+        """
+        Returns a ControlCommand for the given dict.
+        """
+        typ = next(iter(obj))
+        obj = obj[typ]
+        runId = obj['runId']
+        timeout = obj['timeout']
+        return QueryFinal(runId, timeout)
 
 
 class CommandServer:
@@ -45,7 +60,7 @@ class CommandServer:
     """
     crm = CommandResponseManager()
 
-    async def _handleCommand(self, request: Request) -> Response:
+    async def _handlePost(self, request: Request) -> Response:
         obj = await request.json()
         method = next(iter(obj))
         if method in {'Submit', 'Oneway', 'Validate'}:
@@ -63,16 +78,38 @@ class CommandServer:
                 commandResponse = self.handler.validateCommand(runId, command)
             responseDict = commandResponse.asDict()
             return web.json_response(responseDict)
-        elif method == 'Query':
-            pass  # XXX TODO FIXME
         else:
             raise Exception("Invalid Location type: " + method)
 
-    async def _handleQueryFinal(self, request: Request) -> Response:
-        runId = request.match_info['runId']
-        commandResponse = await self.crm.waitForTask(runId)
+    async def _handleQueryFinal(self, queryFinal: QueryFinal) -> Response:
+        commandResponse = await self.crm.waitForTask(queryFinal.runId)  # XXX TODO: timeout
         responseDict = commandResponse.asDict()
         return web.json_response(responseDict)
+
+    async def _handleWs(self, request: Request) -> WebSocketResponse:
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+        msg: WSMessage
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                if msg.data == 'close':
+                    await ws.close()
+                else:
+                    obj = json.loads(msg.data)
+                    method = next(iter(obj))
+                    # '{"QueryFinal":{"runId":"396195e9-34d6-468c-bcfd-6def4b5e74a0","timeout":[20,"SECONDS"]}}'
+                    if method == "QueryFinal":
+                        queryFinal = QueryFinal.fromDict(obj)
+                        resp = await self._handleQueryFinal(queryFinal)
+                        await ws.send_str(resp.text)
+                        await ws.close()
+                    elif method == "SubscribeCurrentState":
+                        pass  # XXX TODO FIXME: Handle subscribe current state
+                        await ws.close()
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print('ws connection closed with exception %s' % ws.exception())
+        print('websocket connection closed')
+        return ws
 
     @staticmethod
     def _registerWithLocationService(prefix: str, port: int):
@@ -86,7 +123,8 @@ class CommandServer:
         self.handler = handler
         app = web.Application()
         app.add_routes([
-            web.post('/post-endpoint', self._handleCommand)
+            web.post('/post-endpoint', self._handlePost),
+            web.get("/websocket-endpoint", self._handleWs)
         ])
         self._registerWithLocationService(prefix, port)
         web.run_app(app, port=port)
