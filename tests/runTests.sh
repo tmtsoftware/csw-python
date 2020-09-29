bf1ec1814c700aa2649892b8ddf4987ca4f03470
abrighto <Allan.Brighton@t-online.de>
2020-09-29 11:29:47 +0200

    Updated for csw-3.0.0-M1

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -3,9 +3,11 @@
 # Script that starts the CSW services, compiles and runs the test assembly and then runs the python tests.
 # Assumes that csw-services.sh, sbt, pytest are all in your shell path.
 
+CSW_VERSION=v3.0.0-M1
+
 logfile=test.log
 set -v
-csw-services.sh --version v3.0.0-M1 start -e > $logfile 2>&1 &
+csw-services.sh --version $CSW_VERSION start -e > $logfile 2>&1 &
 #csw-services.sh start -e > $logfile 2>&1 &
 cd testSupport || exit 1
 sbt clean stage  >> $logfile 2>&1
