bde149744f977c3ebdbb387bd4f48a2bf9f1c9ed
abrighto <Allan.Brighton@t-online.de>
2020-11-06 16:12:07 +0100

    Updated runTests.sh to use new csw-services app (installed with cs).

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -3,16 +3,21 @@
 # Script that starts the CSW services, compiles and runs the test assembly and then runs the python tests.
 # Assumes that csw-services.sh, sbt, pytest are all in your shell path.
 
-CSW_VERSION=v3.0.0-M1
+CSW_VERSION=3.0.0-M1
 
 logfile=test.log
-if ! hash csw-services.sh 2>/dev/null ; then
-    echo >&2 "Please install csw-services.sh (from csw sources or download csw-apps zip from csw GitHub release).  Aborting."
+if ! hash csw-services 2>/dev/null ; then
+    echo >&2 "Please install csw-services (run: cs install csw-services $CSW_VERSION).  Aborting."
     exit 1
 fi
+csw_services_version=`csw-services --help | grep Main | sed -e 's/Main //'`
+if test "$csw_services_version" != "$CSW_VERSION"; then
+    echo >&2 "CSW services version $csw_services_version != $CSW_VERSION: Please install csw-services (run: cs install csw-services $CSW_VERSION).  Aborting."
+    exit 1
+fi
+
 set -v
-csw-services.sh --version $CSW_VERSION start -e > $logfile 2>&1 &
-#csw-services.sh start -e > $logfile 2>&1 &
+csw-services start -e > $logfile 2>&1 &
 cd testSupport || exit 1
 sbt clean stage  >> $logfile 2>&1
 test-deploy/target/universal/stage/bin/test-container-cmd-app --local test-deploy/src/main/resources/TestContainer.conf   >> $logfile 2>&1 &
