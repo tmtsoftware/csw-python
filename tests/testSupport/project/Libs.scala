b45e92f8a03048ec44ecfa0554b6601d62111f3e
Allan Brighton <Allan.Brighton@t-online.de>
2023-10-06 20:34:53 +0200

    Updated for csw-3.0.1

--- tests/testSupport/project/Libs.scala
+++ tests/testSupport/project/Libs.scala
@@ -4,13 +4,6 @@ object Libs {
   val ScalaVersion = "3.3.1"
 }
 
-//object PekkoHttp {
-//  val Version = "1.0.0"
-//  val Org     = "org.apache.pekko"
-//
-//  val `pekko-http-spray-json` = Org %% "pekko-http-spray-json" % Version
-//}
-
 object CSW {
 //  val Version = "5.0.0"
 val Version = "6f29ed1"
