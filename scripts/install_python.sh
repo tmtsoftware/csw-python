d03a58ef04d2b15ec005c53062b595d96fd08e14
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-12 11:37:38 +0200

    Testing Jenkins build

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-test -d $HOME/.pyenv && exit 0
-curl https://pyenv.run | bash
+sudo apt install -y build-essential libssl-dev zlib1g-dev libsqlite3-dev libffi-dev
+
+#test -d $HOME/.pyenv && exit 0
+#curl https://pyenv.run | bash
 pyenv install -v 3.13.3
